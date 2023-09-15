2556c924bb6f7d17bbf6ae12ef9f7c3218fa6115
alexis <alexissimoes94@gmail.com>
2023-09-15 16:56:48 +0200

    v2

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <title>Document</title>
+</head>
+<div class="contenaire">
+        <header>
+            <div>
+                <a href="./connexion.php">
+                    <img src="asset/img/Pokemon-Logo-PNG-Pic.png" class="logo" alt="">
+                </a>
+                <img src="asset/img/International_Pokémon_logo.png" class="img" alt="">
+            </div>
+        </header>
+        <body>
+            <form action="" method="post">
+                <div class="divForm">
+                    <label for="code">code d'entrée</label> <br>
+                    <input type="text" name="code" id="code">
+                </div>
+                <div class="divForm">
+                    <label for="tell">numéro de téléphone</label>
+                    <input type="text" name="tell" id="tell">
+                </div>
+                
+            </form>
+        </body>
+    <footer>
+        
+    </footer>
+</div>
+</html>
